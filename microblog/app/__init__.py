1c8be506e8986f2a696bfacbece7d8ac26f4bf7b
hajaking <953258481@qq.com>
2018-10-12 10:31:31 +0800

    。？

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,9 +1,13 @@
 from flask  import Flask
 #first one is flask package the second one is Class Flask
-
+from  config import Config
 app=Flask(__name__)
 #__name__：python预先设置的模块 表示当前调用它的模块的名字
 #passing __name__ is almost always going to configure Flask in the correct way.
+
+# app.config['SECRET_KEY']='you-will-never-guess'
+#better write in a individe .py
+app.config.from_object(Config)
 
 # from helloworld import routes
 from app import routes

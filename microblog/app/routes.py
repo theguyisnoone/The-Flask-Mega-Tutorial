0ee829f9c0dc1907d4ec7a78e220918b0b28e558
hajaking <953258481@qq.com>
2018-10-15 18:02:15 +0800

    register

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -4,6 +4,8 @@ from app.forms import LoginForm
 from flask_login import current_user,login_user
 from app.models import User
 from flask_login import login_required
+#logout
+from flask_login import logout_user
 #next
 from flask import request
 from  werkzeug.urls import  url_parse
@@ -23,7 +25,7 @@ def index():
      'body':'exhausted'
     }
     ]
-    return render_template('index.html',title='sddd',user=user,posts=posts)
+    return render_template('index.html',title='Home Page',posts=posts)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -31,17 +33,17 @@ def login():
         return redirect(url_for('index'))
     form=LoginForm()
     if form.validate_on_submit():
-        user=User.query.filter_by(usernaem=for m.username.data).first()
+        user=User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invaild username or password')
             return redirect(url_for('login'))
         login_user(user,remember=form.remember_me.data)
         #next
         next_page=request.args.get('next')
-        if  not next_page or url_parse(next_page).netloc != '':
+        if  not next_page or url_parse(next_page).netloc != '':#不含next/绝对路径
              next_page =url('index')
-        return redirect(next_page)     
-        return redirect(url_for('index'))
+        return redirect(next_page)
+
     return render_template('login.html',title='Sign In',form=form)
 
 @app.route('/logout')

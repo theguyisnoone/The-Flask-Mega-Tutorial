8bf4a4da3cfd352a29ce2bc407371f40f8e079d1
hajaking <953258481@qq.com>
2018-11-05 18:09:56 +0800

    application structure

---
+++ flasky/lab7/flasky/app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+
+
+bootstrap = Bootstrap(app)
+mail=Mail(app)
+moment = Moment(app)
+db=SQLAlchemy(app)
+
+
+def create_app(config_name):
+    app=Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    #route &error
+
+    return app

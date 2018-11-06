1042d5b7b6eb135d4a60284ba4614a1529a22e67
hajaking <953258481@qq.com>
2018-11-06 11:13:20 +0800

    xx

--- flasky/lab7/flasky/app/__init__.py
+++ flasky/lab7/flasky/app/__init__.py
@@ -25,4 +25,8 @@ def create_app(config_name):
 
     #route &error
 
+    #blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
--- flasky/lab7/flasky/app/main/__init__.py
+++ flasky/lab7/flasky/app/main/__init__.py
@@ -1,1 +1,6 @@
 #main blueprint
+from flask import Blueprint
+
+main=Blueprint('main',__name__)
+
+from . import views,errors

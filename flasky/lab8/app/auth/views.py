9eca3675d4a5fa3d0d89959d4f78340e0e3242c0
李大王 <lijiaming@lidawangdeMacBook-Air.local>
2018-11-07 16:39:34 +0800

    why?

---
+++ flasky/lab8/app/auth/views.py
@@ -0,0 +1,31 @@
+from flask import render_template,redirect,request,url_for,flash
+from flask_login import login_user
+from flask_login import login_required,logout_user
+from . import auth
+from ..models import User
+from .forms import LoginForm
+
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.email.data):
+            login_user(user,form.remember_me.data)
+
+            next=request.args.get('next')
+            if next is None or not next.startwith('/'):
+                next=url_for('main.index')
+            return redirect(next)
+
+        flash('Invalid username or password')
+
+    return render_template('auth/login.html',form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))

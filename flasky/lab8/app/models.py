b3d76a90e80436751b387b569e9ab2fa2211abd9
李大王 <lijiaming@lidawangdeMacBook-Air.local>
2018-11-14 14:28:20 +0800

    xx

--- flasky/lab8/app/models.py
+++ flasky/lab8/app/models.py
@@ -70,11 +70,12 @@ class User(UserMixinm,db.Model):
 
     def __init__(self,**kwargs):
         super(User,self).__init__(**kwargs)
-        if self.role is None:;
-            if self.email == current_app.config['FLASKY_ADMIN']:
+        if self.role is None:
+            # if self.email == current_app.config['FLASKY_ADMIN']:
+            if self.email == '953258481@qq.com':
                 self.role=Role.query.filter_by(name='Adiministrator').first()
             if self.role is None:
-                self.role=Role.query.filter_by(default=True').first()
+                self.role=Role.query.filter_by(default='True').first()
 
     def __repr__(self):
         return '<User {}>' .format(self.username)
